44031878d2e4675a4ccddb881c39a3bf93d77e85
Richardh Harper <dick.harper@gmail.com>
2023-03-12 16:35:38 -0600

    WIP Strategy pattern.

---
+++ Strategy/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace DesingPatterns.Behavioral.Strategy;
+
+/*
+ *  Summary:
+ *      The strategy pattern is a behavioral software design strategy that 
+ *      allows an algorithm's behavior to be selected at runtime. 
+ *      The pattern encapsulates each algorithm and makes the algorithm
+ *      interchangeable. This pattern promotes the Open/Closed principle
+ *      by using composition over inheritance.
+ *      
+ *  Problem:
+ *      
+ *  Solution:
+ *      The strategy pattern simplifies and accelerates development by 
+ *      outsourcing complex algorithms to separate classes. This can be 
+ *      demonstrated by using the strategy pattern to encapsulate 
+ *      different sorting algorithms.
+ *      
+ *  Add the following classes:    
+ *      1. ISortStrategy
+ *      
+ */
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        
+
+        
+        
+    }
+}
+
